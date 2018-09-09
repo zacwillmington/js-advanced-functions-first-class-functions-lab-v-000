7f7dc6cbe564b323585d52fe7408bc69a648ced1
Zac Willmington <zac.willmington@gmail.com>
2018-09-09 21:55:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
 function returnFirstTwoDrivers(drivers){
-    
+    // for (const driver in drivers) {
+    //     if (){}
+    // }
+    const firstTwo = drivers.slice(0, 1);
+    return firstTwo;
 }
